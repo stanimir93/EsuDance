2c32cc09105542fe609ba8bd53758a31ee103eb7
Your Name <stanimir.nikolov123@gmail.com>
2021-08-19 15:24:12 +0100

    test
    test - delete it

--- js/global.js
+++ js/global.js
@@ -11,31 +11,4 @@ fetch('json/data.json')
 }));
 
 
-// MutationObserver to listen for loading of the footer
 
-const observer = new MutationObserver( function() {
-    loadAddress();
-    loadEmail();
-    this.disconnect();
-})
-
-observer.observe(document.querySelector('footer'),{childList: true})
-
-
-// Functions to populate addess fields
-
-function loadAddress() {
-    document.querySelectorAll('.address').forEach( (addressContainer) => {
-        addressContainer.innerHTML = 
-        `${contactDetails.address_line1}<br>${contactDetails.street}<br>${contactDetails.postcode}<br>${contactDetails.city}`;
-    })
-}
-
-// Function to populate email fields
-
-function loadEmail() {
-    document.querySelectorAll('.email').forEach( (emailContainer) => {
-        emailContainer.textContent = `${contactDetails.email}`;
-        emailContainer.setAttribute('href', `mailto:${contactDetails.email}`)
-    })
-}
